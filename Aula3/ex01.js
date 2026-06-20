620ee8e620623bd953351ed55fcf5d1931a1ff72
Ismael-pereira <ismael.valdoni.pereira@escola.pr.gov.br>
2026-06-20 13:06:35 -0300

    ex01.js
    Define a person object and log its properties. Update the city property and log the updated object.

---
+++ Aula3/ex01.js
@@ -0,0 +1,22 @@
+let pessoa = {
+    nome: "Ismael Valdoni Pereira",
+    idade: 15,
+    cidade: "Cascavel",
+    curso: "Desenvolvimento de Sistema"
+};
+
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`Idade: ${pessoa.idade}`);
+console.log(`Cidade: ${pessoa.cidade}`);
+console.log(`Curso: ${pessoa.curso}`);
+
+pessoa.cidade = "Toledo";
+
+console.log("\nApós alteração:\n");
+
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`Idade: ${pessoa.idade}`);
+console.log(`Cidade: ${pessoa.cidade}`);
+console.log(`Curso: ${pessoa.curso}`);
+
+//OBS: professor eu não entendi, Estou sendo Honesto, e usei IA nessa aqui para Tentar entender desculpa 🥲
